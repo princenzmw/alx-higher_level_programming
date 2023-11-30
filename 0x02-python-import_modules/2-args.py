2abb0c35180dc75e187994e7d48ebdbe01b50206
princenzmw <princenzmw@gmail.com>
2023-12-01 00:11:02 +0300

    complete task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+
 import sys
-
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    plural = "s" if argc != 1 else ""
-    separator = ":" if argc > 0 else "."
+    argc = len(sys.argv)
+    argv = sys.argv
 
-    print("{} argument{}{}".format(argc, plural, separator))
+    if argc < 2:
+        print("0 arguments.")
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
