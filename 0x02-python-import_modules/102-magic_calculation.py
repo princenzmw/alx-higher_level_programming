4bbdd036301a21c07bdf33810c483613c9708115
princenzmw <princenzmw@gmail.com>
2023-12-01 02:31:16 +0300

    complete task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-from magic_calculation_102 import add, sub
-
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         return add(a, b) + sum(range(4, 6))
     else:
