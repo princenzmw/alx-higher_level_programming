aa38e081239ec6ebb70dd1b530a2df587d4c0788
princenzmw <princenzmw@gmail.com>
2023-12-02 07:41:45 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tap_a = tuple_a
-    new_tap_b = tuple_b
+    if tuple_a and tuple_b:
+        new_tap_a = tuple_a
+        new_tap_b = tuple_b
 
-    if len(tuple_a) < 2:
-        new_tap_a = tuple_a + (0,) * (2 - len(tuple_a))
-    elif len(tuple_b) < 2:
-        new_tap_b = tuple_b + (0,) * (2 - len(tuple_b))
+        if len(tuple_a) < 2:
+            new_tap_a = tuple_a + (0,) * (2 - len(tuple_a))
+        elif len(tuple_b) < 2:
+            new_tap_b = tuple_b + (0,) * (2 - len(tuple_b))
 
-    result1 = new_tap_a[0] + new_tap_b[0]
-    result2 = new_tap_a[1] + new_tap_b[1]
-    return result1, result2
+        result1 = new_tap_a[0] + new_tap_b[0]
+        result2 = new_tap_a[1] + new_tap_b[1]
+        return result1, result2
