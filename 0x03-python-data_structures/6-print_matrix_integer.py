c419ea75784e5ba3be5e593a7f89b2286e7cb824
princenzmw <princenzmw@gmail.com>
2023-12-02 07:15:56 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for num_list in matrix:
         for num in num_list:
-            print("{}".format(num), end=" " if num is not
+            print("{:d}".format(num), end=" " if num is not
                   num_list[len(num_list) - 1] else '')
         else:
             print()
