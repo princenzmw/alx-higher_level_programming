e961f4ebbdecaa6a90f3d787ee3daa652be3b957
princenzmw <princenzmw@gmail.com>
2023-12-06 20:04:47 +0300

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    keys = a_dictionary.keys()
+
+    if key in keys:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
 
     return a_dictionary
