4b7e4a86ae0e9cce203b10dc053851681d44bcf9
princenzmw <princenzmw@gmail.com>
2023-12-06 18:23:47 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,15 @@
 def search_replace(my_list, search, replace):
     if my_list is None:
         my_list = []
-    
-    for i, value in enumerate(my_list):
-        if value == search:
-            my_list[i] = replace
-        else:
-            my_list[i] = value
+    if search not in my_list:
+        return my_list
 
-    return my_list
+    new_list = my_list.copy()
+
+    for i, value in enumerate(new_list):
+        if value == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = value
+
+    return new_list
