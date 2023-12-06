49884b0fa80e96fe7bdbe7758b197ed6dc00d161
princenzmw <princenzmw@gmail.com>
2023-12-06 17:41:51 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [x for x in my_list]
+    new_list = my_list.copy()
     
     for i in range(len(new_list)):
         if new_list[i] == search:
