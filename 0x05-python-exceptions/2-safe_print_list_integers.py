c871699a203a5d197c79c093cd08e5145a9c58e1
princenzmw <princenzmw@gmail.com>
2023-12-18 16:07:32 +0300

    Task 2 complete

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            else:
+                continue
+    except TypeError:
+        pass
+
+    print()
+    return count
