728ee8bc6612c8e146af866f42d5be09fce5ab9f
princenzmw <princenzmw@gmail.com>
2023-12-18 21:24:31 +0300

    complete task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as te:
+    except (ValueError, TypeError) as te:
         print("Exception: {}".format(te), file=sys.stderr)
         return False
