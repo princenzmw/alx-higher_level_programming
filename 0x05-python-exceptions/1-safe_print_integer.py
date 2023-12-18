3b43fff12245117d48c1f7a56ba1b6259a6ef74f
princenzmw <princenzmw@gmail.com>
2023-12-18 14:30:19 +0300

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    status = False
+
+    try:
+        print("{:d}".format(value))
+        status = True
+    except:
+        status = False
+
+    return status
