3d6d10944b58504678ddb94561844e26abae3378
princenzmw <princenzmw@gmail.com>
2023-12-18 21:37:46 +0300

    task 8 complete

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (TypeError, ValueError, ZeroDivisionError, IndexError) as fe:
+        print(f"Exception: {fe}", file=sys.stderr)
+        return None
