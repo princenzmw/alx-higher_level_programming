cff9ced5a5d017f8086705933907449752f08577
princenzmw <princenzmw@gmail.com>
2023-12-20 02:54:10 +0300

    working on task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+class Square:
+    """Represents a square shape.
+
+    Attributes:
+        __size: Private instance attribute to store the size of the square.
+
+    Methods:
+        None
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
