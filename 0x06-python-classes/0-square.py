1cab3d584107741dd053662878cf74c3b4698a26
princenzmw <princenzmw@gmail.com>
2023-12-20 01:59:27 +0300

    task 0 maybe

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""A class representing a square.
+
+This class is currently empty and can be used as
+a placeholder for future implementations related to squares.
+
+Arguments:
+    None
+
+Methods:
+    None
+"""
+
 
 class Square:
-    """A class representing a square.
-
-    This class is currently empty and can be used as
-    a placeholder for future implementations related to squares.
-
-    Arguments:
-        None
-
-    Methods:
-        None
-    """
-
     pass
