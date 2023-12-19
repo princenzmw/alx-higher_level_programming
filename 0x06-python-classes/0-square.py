b239e1e8033527853b9e8e959debff2162082b8d
princenzmw <princenzmw@gmail.com>
2023-12-20 01:34:45 +0300

    task 0 completed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """A class representing a square.
-
-    This class is currently empty and can be used as
-    a placeholder for future implementations related to squares.
-
-    Arguments:
-        None
-
-    Methods:
-        None
-    """
+    """An empty class to define a square. """
+    pass
