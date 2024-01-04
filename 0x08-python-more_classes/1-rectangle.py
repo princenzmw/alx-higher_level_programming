a4ecf3602acc563db538a836645152b7473338a3
princenzmw <princenzmw@gmail.com>
2024-01-05 01:03:08 +0200

    let me try this one

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,23 +4,45 @@
 
 
 class Rectangle:
-    """ Defining a rectangle """
+    """
+    Rectangle class represents a rectangle with width and height.
+    """
 
     def __init__(self, width=0, height=0):
-        """ class constructor """
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int): Optional width of the rectangle (default is 0).
+            height (int): Optional height of the rectangle (default is 0).
+        """
 
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """ property to reteieve width """
+        """
+        Getter method for retrieving the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
 
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ property setter for the height """
+        """
+        Setter method for setting the width of the rectangle.
+
+        Args:
+            value (int): The width to set.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If the width is less than 0.
+        """
 
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
@@ -31,7 +53,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """ property to reteieve height """
+        """
+        Getter method for retrieving the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
 
         return self.__height
 
