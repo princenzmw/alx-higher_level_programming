50b214bbe3702d5f016a187d38d68454fc90e492
princenzmw <princenzmw@gmail.com>
2024-01-05 00:47:16 +0200

    maybe it's the problem with the docstring (checker 10, 11 failed)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,17 +8,20 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ class constructor """
+
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
         """ property to reteieve width """
+
         return self.__width
 
     @width.setter
     def width(self, value):
         """ property setter for the height """
+
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
@@ -29,11 +32,14 @@ class Rectangle:
     @property
     def height(self):
         """ property to reteieve height """
+
         return self.__height
 
     @height.setter
     def height(self, value):
+
         """ property setter for the height """
+
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 0:
