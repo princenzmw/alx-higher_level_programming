f83d0c57b8ea9a9b7ed93dd32cf42ca0124dd1a3
princenzmw <princenzmw@gmail.com>
2024-01-05 01:06:49 +0200

    Changing the width and heigt attributes in the init function

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
             height (int): Optional height of the rectangle (default is 0).
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
