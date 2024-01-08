a21589cc79ea16b4463b09c5486494d01daf449f
princenzmw <princenzmw@gmail.com>
2024-01-08 22:33:00 +0200

    task 0 file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+def add_integer(a, b=98):
+    """_summary_
+
+    Args:
+        a (int): First value for the addition. must be integer or float
+        b (int, optional): Second value for the addition. must be integer or float. Defaults to 98.
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int: addition of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
