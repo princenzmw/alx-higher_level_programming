1eb3e54945ed93b891f41ffdaf8cda37449d3366
princenzmw <princenzmw@gmail.com>
2024-01-10 01:38:48 +0200

    Fifth task initial function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 """ A function that writes an Object to a text file,
     using a JSON representation """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an Object to a text file,
+    using a JSON representation
+
+    Args:
+        my_obj (obj): a Python object
+        filename (file): a file-like object
+
+    Returns:
+        JSON : Write a Python object as a JSON string to a file-like object
+    """
+    with open(filename, "r", encoding='utf-8') as file:
+        return json.dump(my_obj, file)
