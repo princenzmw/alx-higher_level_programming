2b3659e44181db1a43a2bae7bd7a194de03e4c6b
princenzmw <princenzmw@gmail.com>
2024-01-09 23:52:43 +0200

    Added the head documentation to task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+""" A function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object """
