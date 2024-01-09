67457dac3b292282eb6c92e0ab98865bf833830d
princenzmw <princenzmw@gmail.com>
2024-01-10 00:35:20 +0200

    initial function check

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """ A function that writes a string to a text file (`UTF8`)
     and returns the number of characters written """
+
+
+def write_file(filename="", text=""):
+    with open(filename, '+a', encoding='utf-8') as f:
+        f.write(text)
+    return len(text)
