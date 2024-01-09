578288c44c91b3164580b85b403e20ef43c8fc3f
princenzmw <princenzmw@gmail.com>
2024-01-10 01:27:44 +0200

    Task 4 initial function

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 """ A function that returns an object (Python data structure)
     represented by a JSON string """
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Return a Python data structure represented by a JSON string.
+
+    Args:
+        my_str (JSON): a JSON string
+
+    Returns:
+        _type_: A Python data structure
+    """
+    return json.loads(my_str)
