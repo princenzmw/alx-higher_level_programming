05009ad842b3e48225ae7a86f3e7ffccf253ea71
princenzmw <princenzmw@gmail.com>
2024-01-09 23:51:53 +0200

    Added the head documentation to task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" A  function that reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ read a file
+
+    Args:
+        filename (str, optional): a file's name. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        file_cont = f.read()
+    
+    print(file_cont)
