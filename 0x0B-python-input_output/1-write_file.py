ca978dde51fc869b4fe63ac227dbc52bee3a0dab
princenzmw <princenzmw@gmail.com>
2024-01-10 00:49:07 +0200

    added docString to my function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,15 @@
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file (`UTF8`)
+    and returns the number of characters written
+
+    Args:
+        filename (str, optional): file name. Defaults to "".
+        text (str, optional): string to write. Defaults to "".
+
+    Returns:
+        int: the number of characters written
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
