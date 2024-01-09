c960ab65bc0b7312a30183dcde30e0262af6b678
princenzmw <princenzmw@gmail.com>
2024-01-10 01:14:15 +0200

    function initial definition

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """ A function that returns the JSON representation of an object (string) """
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object
+
+    Args:
+        my_obj (object): a Python object (data structure)
+
+    Returns:
+        str: The JSON representation of a passed object
+    """
+    return json.dumps(my_obj)
