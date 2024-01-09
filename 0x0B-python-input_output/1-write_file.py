4c64c6116048811df5ee6368b8e4b7bf4af616c8
princenzmw <princenzmw@gmail.com>
2024-01-10 00:42:04 +0200

    change the mode from append+ to write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, '+a', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(text)
     return len(text)
