c8540f927f2712b8749156e15ac2aebeba9e209b
princenzmw <princenzmw@gmail.com>
2024-01-10 02:55:06 +0200

    moved the imported functions in the if statement

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,15 @@
 
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__(
+        "6-load_from_json_file"
+    ).load_from_json_file
     arguments = sys.argv
     data = list(arguments[1:])
 
     save_to_json_file(data, "add_item.json")
-    load_from_json_file('add_item.json')
+    load_from_json_file("add_item.json")
