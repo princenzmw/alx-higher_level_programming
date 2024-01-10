22310da03b77f8568042578296901c0d6a22701b
princenzmw <princenzmw@gmail.com>
2024-01-10 03:18:32 +0200

    Twelth task's code

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 """ A function `def pascal_triangle(n):` that returns a list of lists
     of integers representing the Pascal’s triangle of `n` """
+
+
+def pascal_triangle(n):
+    """Represents Pascal's Triangle of size n"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
