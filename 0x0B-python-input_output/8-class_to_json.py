5bced1c48329e91d7f10d1c9c63022e2dc208f0d
princenzmw <princenzmw@gmail.com>
2024-01-10 03:17:09 +0200

    Eighth task's initial function

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,3 +2,8 @@
 """ A function that returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object """
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
