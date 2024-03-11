960f2b5fc45722cb5d031bc39e094d811f8b54a2
princenzmw <princenzmw@gmail.com>
2024-03-11 22:15:10 +0200

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 
-function incr () {
-  this.value += 1;
-}
-
-myObject.incr = incr;
+myObject.incr = function () {
+  this.value++;
+};
 
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
