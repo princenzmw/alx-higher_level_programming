bdccf778a0c10ae1ee071636c557de9486e9b8fb
princenzmw <princenzmw@gmail.com>
2024-03-11 12:17:22 +0200

    Task 11. Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+const myArgs = argv.slice(2);
+const myArray = [];
+
+myArgs.forEach(a => {
+  myArray.push(parseInt(a));
+});
+
+const sorted = myArray.sort((a, b) => b - a);
+
+if (myArgs.length <= 1) {
+  console.log(0);
+} else {
+  console.log(sorted);
+}
