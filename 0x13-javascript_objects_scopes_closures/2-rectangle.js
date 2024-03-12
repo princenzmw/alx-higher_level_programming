6fa9e84b6ab42cdf4639868adbe57b64ce44206d
princenzmw <princenzmw@gmail.com>
2024-03-12 10:48:04 +0200

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (!(isNaN(w) || w < 1 || isNaN(h) || h < 1)) {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
     }
+    this.width = w;
+    this.height = h;
   }
 }
 
