90c245c939c84c5366d39e8309145f5d75610dcb
princenzmw <princenzmw@gmail.com>
2024-03-13 11:10:11 +0200

    Square #1 fixed the code The `double` method was supposed to double the size of the square, but it it was not functioning as expected.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,29 +1,19 @@
 #!/usr/bin/node
-const Squar = require('./5-square');
+const SquareBase = require('./5-square');
 
-class Square extends Squar {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
+class Square extends SquareBase {
+  // Constructor is removed because it's redundant
 
   charPrint (c) {
     if (!c) {
-      for (let i = 0; i < this.size; i++) {
-        let row = '';
-        for (let j = 0; j < this.size; j++) {
-          row += 'X';
-        }
-        console.log(row);
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
       }
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        let row = '';
-        for (let j = 0; j < this.size; j++) {
-          row += c;
-        }
-        console.log(row);
-      }
+      console.log(row);
     }
   }
 }
