5c66211a6440940808ee1b386abdbc20939676b4
princenzmw <princenzmw@gmail.com>
2024-03-13 05:22:38 +0200

    Task 12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,25 @@
-#!/usr/bin/bnode
+#!/usr/bin/node
+
+// Import the dictionary from 101-data.js file
+const data = require('./101-data').dict;
+
+// Create a new empty dictionary to store occurrences as keys and user ids as values
+const occurrencesById = {};
+
+// Loop through each user id in the imported dictionary
+for (const userId in data) {
+  // Get the number of occurrences for this user id
+  const occurrence = data[userId];
+
+  // If this is the first time we see this number of occurrences,
+  // create a new array to hold user ids
+  if (!occurrencesById[occurrence]) {
+    occurrencesById[occurrence] = [];
+  }
+
+  // Add the current user id to the array for this number of occurrences
+  occurrencesById[occurrence].push(userId);
+}
+
+// Print the new dictionary
+console.log(occurrencesById);
