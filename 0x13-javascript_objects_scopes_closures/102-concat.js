c58b74e973d962f3206195f32015952c90187760
princenzmw <princenzmw@gmail.com>
2024-03-13 05:33:07 +0200

    Task 13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,19 @@
-#!/usr/bin/bnode
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get file paths from the arguments passed to the script
+const fileAPath = process.argv[2];
+const fileBPath = process.argv[3];
+const fileCPath = process.argv[4];
+
+// Read the content of the first file
+const contentA = fs.readFileSync(fileAPath, { encoding: 'utf8', flag: 'r' });
+
+// Read the content of the second file
+const contentB = fs.readFileSync(fileBPath, { encoding: 'utf8', flag: 'r' });
+
+// Concatenate the contents of both files
+const combinedContent = contentA + contentB;
+
+// Write the concatenated content to the destination file
+fs.writeFileSync(fileCPath, combinedContent);
