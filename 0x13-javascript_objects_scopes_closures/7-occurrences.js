8404bfc2b87780a3bec04b27e08c7ad75c755470
princenzmw <princenzmw@gmail.com>
2024-03-13 04:25:16 +0200

    Task 7. Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/bnode
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
