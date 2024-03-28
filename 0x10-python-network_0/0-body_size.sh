ead677db6e4390fe98c11165f51852917ff3155e
princenzmw <princenzmw@gmail.com>
2024-03-28 02:25:59 +0200

    shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
-# Get the comtent-lenght of a giving ip address
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays
+#+	the size of the body of the response (in bytes) using 'curl'
 curl -sI "$1" | awk '/Content-Length/{print $2}'
