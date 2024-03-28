b6d54cea0caebd3efcaf12e6521f3101a3b2e792
princenzmw <princenzmw@gmail.com>
2024-03-28 02:39:27 +0200

    shellcheck

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,6 @@
 # This script takes in a URL, sends a request to that URL
 # and displays the size of the body of the response in bytes
 
-url=$1
-total_size=$(curl -sI $url | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-echo $total_size
+url="$1"
+total_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
+echo "$total_size"
