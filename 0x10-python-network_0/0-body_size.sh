dfe81b0f9a2e2d52494ef43be1df1db357b50e06
princenzmw <princenzmw@gmail.com>
2024-03-28 09:27:06 +0200

    number of lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL
-# and displays the size of the body of the response in bytes
-
-url="$1"
-total_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-if [ -z "$total_size" ]
-then
-      total_size=0
-fi
-echo "$total_size"
+# Bash script takes in a URL, sends a request to that URL and displays the size of the body (comtent-lenght) of the response in bytes
+curl -sI "$1" | awk '/Content-Length/{print $2}'
