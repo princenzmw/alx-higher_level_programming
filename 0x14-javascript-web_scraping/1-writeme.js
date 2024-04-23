08b97866b38a5a41452c8c641cbe230a91721d8c
princenzmw <princenzmw@gmail.com>
2024-04-23 12:40:52 +0200

    Write me - a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 // A Node script that writes a string to a file.
 
+// import the fs (file system) module
 const fs = require('fs');
+
+// Get the command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// Write the data to file
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(content);
   }
 });
